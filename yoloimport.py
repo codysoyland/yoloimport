dada7bad22e223ea20e1089e9ef976a2ab8136b8
Matt Jaffee <jaffee@umbel.com>
2015-04-01 20:04:08 -0500

    automatically install mock if user doesn't have it installed

--- yoloimport.py
+++ yoloimport.py
@@ -1,5 +1,4 @@
 import os
-import mock
 
 maybe_import = __import__
 
@@ -16,6 +15,12 @@ def yoloimport(name, *args, **kwargs):
             return maybe_import(name, *args, **kwargs)
         except ImportError:
             # Silly package doesn't exist. Who needs a stupid ImportError? Let's give the caller a mock object... they can do anything!
+            try:
+                mock = maybe_import("mock")
+            except ImportError:
+                # If the user doesn't have mock installed... well - we know how to handle that
+                os.system('pip install mock')
+                mock = maybe_import("mock")
             return mock.MagicMock()
 
 __builtins__['__import__'] = yoloimport
