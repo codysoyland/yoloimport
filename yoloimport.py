4d4040e8142c81f52f4e98bde617fea50b8b8193
Matt Jaffee <jaffee@umbel.com>
2015-04-01 20:34:06 -0500

    let's get recursive

--- yoloimport.py
+++ yoloimport.py
@@ -8,19 +8,22 @@ def yoloimport(name, *args, **kwargs):
         return maybe_import(name, *args, **kwargs)
     except ImportError:
         # Oops, maybe it's not installed? Let's fix that!
+
         os.system('pip install ' + name.split('.')[0])
 
         # Let's try again!
         try:
             return maybe_import(name, *args, **kwargs)
         except ImportError:
-            # Silly package doesn't exist. Who needs a stupid ImportError? Let's give the caller a mock object... they can do anything!
-            try:
-                mock = maybe_import("mock")
-            except ImportError:
-                # If the user doesn't have mock installed... well - we know how to handle that
-                os.system('pip install mock')
-                mock = maybe_import("mock")
-            return mock.MagicMock()
+            # dang
+            if name == "mock":
+                return None
+            else:
+                # we couldn't install the package - let's return a
+                # mock object - they're pretty useful!
+                mock = yoloimport("mock")
+                if mock is not None:
+                    return mock.MagicMock()
+
 
 __builtins__['__import__'] = yoloimport
